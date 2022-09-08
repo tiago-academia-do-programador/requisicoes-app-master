e50b52664387a7c3df5f521e197d7060c79cdf16
Tiago <tiagosantiniathayde@gmail.com>
2022-09-08 11:23:58 -0300

    Implementando CRUD de Minhas Requisições

---
+++ src/app/requisicoes/models/requisicao.model.ts
@@ -0,0 +1,18 @@
+import { Departamento } from "src/app/departamentos/models/departamento.model";
+import { Equipamento } from "src/app/equipamentos/models/equipamento.model";
+import { Funcionario } from "src/app/funcionarios/models/funcionario.model";
+
+export class Requisicao {
+  id: string;
+  descricao: string;
+  dataAbertura: Date | any;
+
+  funcionarioId: string;
+  funcionario?: Funcionario;
+
+  departamentoId: string;
+  departamento?: Departamento;
+
+  equipamentoId?: string;
+  equipamento?: Equipamento;
+}
