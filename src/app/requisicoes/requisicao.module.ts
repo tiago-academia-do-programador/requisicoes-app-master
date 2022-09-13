662961fd5f733311163b166a1aa544d57dc7ed38
Tiago <tiagosantiniathayde@gmail.com>
2022-09-13 00:53:35 -0300

    Refatorando componente de detalhes da requisição

--- src/app/requisicoes/requisicao.module.ts
+++ src/app/requisicoes/requisicao.module.ts
@@ -8,13 +8,15 @@ import { NgSelectModule } from '@ng-select/ng-select';
 import { RequisicoesFuncionarioComponent } from './requisicoes-funcionario/requisicoes-funcionario.component';
 import { RequisicoesDepartamentoComponent } from './requisicoes-departamento/requisicoes-departamento.component';
 import { DetalhesComponent } from './detalhes/detalhes.component';
+import { RequisicaoDetalhesComponent } from './detalhes/requisicao-detalhes/requisicao-detalhes.component';
 
 @NgModule({
   declarations: [
     RequisicaoComponent,
     RequisicoesFuncionarioComponent,
     RequisicoesDepartamentoComponent,
-    DetalhesComponent
+    DetalhesComponent,
+    RequisicaoDetalhesComponent
   ],
   imports: [
     CommonModule,
